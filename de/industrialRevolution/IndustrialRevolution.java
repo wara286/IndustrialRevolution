489ff46bb35fc501ab07bcbb08f5304121153e72
websitenmaker12 <eidechse12@hotmail.com>
2013-09-03 18:08:35 +0200

    Basic

---
+++ de/industrialRevolution/IndustrialRevolution.java
@@ -0,0 +1,10 @@
+package de.industrialRevolution;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@Mod(modid = "industrialRevolution", name = "Industrial Revolution", version = "v0.1")
+@NetworkMod(clientSideRequired = true)
+public class IndustrialRevolution {
+
+}
